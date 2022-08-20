3069a31a3987b601c49a636cf6b5d52402d42d8a
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-21 04:14:01 +0600

    setTimeout()

--- timeout.js
+++ timeout.js
@@ -1,4 +1,15 @@
+const doSome = () => {
+    console.log('coding is faaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaan')
+}
+
 console.log('First: ami sobar age')
 console.log('secound: i am secound')
+setTimeout(doSome, 5000)
+setTimeout(function () {
+    console.log('I am using vs code')
+}, 7000)
+setTimeout(() => {
+    console.log('Exploring MDN articals')
+}, 10000);
 console.log('Third: i am third')
 console.log('Fourth: i am  fourth')
